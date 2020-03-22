d21ad99e3bc96f07e64962adc4e4d0072035cbfa
Chen Sihan <schen@lcs.on.ca>
2020-03-22 17:23:59 -0400

    reusable cell and number of rows in section, adding start.txt and convert it into string and separate it to put them in an array, start game function

--- HackingWithSwiftProject5/ViewController.swift
+++ HackingWithSwiftProject5/ViewController.swift
@@ -9,12 +9,68 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    
+    // Properties
+    var allWords = [String]()
+    
+    // Player's answers
+    var usedWords = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Find the path for start.txt
+        // Use if let because all the values here are optional
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            
+            // try? means if there is any error, return a nil value
+            // There is always a result, but we need to deal with it carefully
+            // Separate the words in the file after convert them into string
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                
+                allWords = startWords.components(separatedBy: "\n")
+                
+            }
+            
+            // isEmpty is more code efficient than .count == 0
+            if allWords.isEmpty {
+                allWords = ["silkworm"]
+            }
+            
+            startGame()
+            
+        }
     }
-
-
+    
+    
+    func startGame() {
+        title = allWords.randomElement()
+        
+        // Keep Capacity is very efficient when you are trying to grow the collection again
+        
+        usedWords.removeAll(keepingCapacity: true)
+        
+        // Calling numberOfRowsInSection and cellForRowAt again
+        
+        tableView.reloadData()
+    }
+    
+    
+    // Two functions to show titles on the cell (number of rows and reusable cell)
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier:
+            "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+    
+    
 }
 
