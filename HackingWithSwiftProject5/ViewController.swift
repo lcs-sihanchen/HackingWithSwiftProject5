97881e2433e064dff26e382950c3ce3a88b0848e
Chen Sihan <schen@lcs.on.ca>
2020-03-24 15:38:23 -0400

    Fix a minor bug (bonus challenge), repetitive word should not exist

--- HackingWithSwiftProject5/ViewController.swift
+++ HackingWithSwiftProject5/ViewController.swift
@@ -168,7 +168,7 @@ class ViewController: UITableViewController {
                     showErrorMessage(errorTitle: "Not long enough", errorMessage: "The word should have more than 2 letters.")
                     
                 } else if isReal(word: lowerAnswer){
-                    usedWords.insert(answer, at: 0)
+                    usedWords.insert(lowerAnswer, at: 0)
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath],
                                          with: .automatic)
